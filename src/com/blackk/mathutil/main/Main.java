788dd25bc03e1ece3d40e29e0e0a4850cedf44f2
kkcalb28 <kut.killer2@gmail.com>
2022-02-17 13:45:55 +0700

    The 1st

---
+++ src/com/blackk/mathutil/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.blackk.mathutil.main;
+import com.blackk.mathutil.core.MathUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        //CASE #1: Check 0!
+        long expected = 1;
+        long actual = MathUtil.getFactorial(0);
+        System.out.println("Check 0! expected:" + expected + "; actual:" +actual);
+        //CASE #2: Check 6!
+        expected = 720;
+        actual = MathUtil.getFactorial(6);
+        System.out.println("Check 6! expected:" + expected + "; actual:" +actual);
+        //CASE #3: Check exception (-5!)
+        actual = MathUtil.getFactorial(-5);
+    }
+}
